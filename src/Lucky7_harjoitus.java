1815dd5468772866a818fd22b355d62e571f83b5
Maiqqe <maisa.tuomenpuro@student.hamk.fi>
2023-10-18 18:16:20 +0300

    Tiedoston luonti ja alku printti

---
+++ src/Lucky7_harjoitus.java
@@ -0,0 +1,13 @@
+
+public class Lucky7_harjoitus {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Tervetuloa pelaamaan Lucky7! Peli arpoo 3 numeroa väliltä 1-10.");
+		System.out.println("Yhdestä 7 voitat 2€, kahdesta 4 € ja kolmesta 7€.");
+		System.out.println("Syötä ensin rahasumma.");
+
+	}
+
+}
